5778035f9be945cc9b75f61ab9eb52687c970829
Farid Huseynov <hferid23@gmail.com>
2024-03-09 23:33:17 +0100

    Make bid, pass bid, and some more funcs

--- thousand-api/database.py
+++ thousand-api/database.py
@@ -1,6 +1,6 @@
 """TODO: Write docstring"""
 
-from model import Base, Player, Round
+from model import Base, Game, Player, Round
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -25,16 +25,29 @@ def update_players_db(session, players):
     session.commit()
 
 
-def get_current_round_from_db(session, game_id):
+def get_current_round_from_db(session, game_id) -> Round:
     """TODO: Write docstring"""
-    return (
-        session.query(Round)
-        .filter_by(game_id=game_id)
-        .order_by(Round.id.desc())
-        .first()
-    )
+    game_obj = session.query(Game).filter_by(id=game_id).first()
+    return session.query(Round).filter_by(game_id=game_id, round_number=game_obj.current_round).first()
 
 
-def get_player_from_db(session, player_id):
+def get_player_from_db(session, player_id: str) -> Player:
     """TODO: Write docstring"""
     return session.query(Player).filter_by(id=player_id).first()
+
+
+def get_player_with_game_and_local_id_from_db(session, game_id: str, player_local_id: int) -> Player:
+    """TODO: Write docstring"""
+    game = session.query(Game).filter_by(id=game_id).first()
+    player_id = str(getattr(game, f"player{player_local_id}_id"))
+
+    return get_player_from_db(session, player_id)
+
+
+def get_players_with_game_from_db(session, game: Game) -> list[Player]:
+    """Gets list of 3 players of a game with given id"""
+    player0 = get_player_from_db(session, game.player0_id)
+    player1 = get_player_from_db(session, game.player1_id)
+    player2 = get_player_from_db(session, game.player2_id)
+
+    return [player0, player1, player2]
--- thousand-api/game.py
+++ thousand-api/game.py
@@ -5,7 +5,12 @@ import uuid
 from datetime import datetime
 from typing import List
 
-from database import Session, get_current_round_from_db
+from database import (
+    Session,
+    get_current_round_from_db,
+    get_player_from_db,
+    get_players_with_game_from_db,
+)
 from model import *
 from model import CardNumber, CardSuit, Game, Player, Round
 
@@ -16,9 +21,9 @@ def create_game(game_id: str, players_ids: List[str]):
 
     game = Game(
         id=game_id,
-        player1_id=players_ids[0],
-        player2_id=players_ids[1],
-        player3_id=players_ids[2],
+        player0_id=players_ids[0],
+        player1_id=players_ids[1],
+        player2_id=players_ids[2],
         creation_date=str(datetime.now()),
     )
 
@@ -60,24 +65,23 @@ def start_round(game_id: str):
     if not game:
         session.close()
         return "Game not found"
-    game.game_state = GameState.PLAYING.value
+    game.game_state = GameState.BIDDING.value
     game.current_round = game.current_round + 1
     session.add(game)
 
-    players = (
-        session.query(Player)
-        .filter(Player.id.in_([game.player0_id, game.player1_id, game.player2_id]))
-        .all()
-    )
+    players = session.query(Player).filter(Player.id.in_([game.player0_id, game.player1_id, game.player2_id])).all()
     players, talon = deal_cards(players)
 
     for player in players:
         session.add(player)
     session.commit()
 
-    round_obj = Round(
-        id=str(uuid.uuid4()), round_number=game.current_round, game_id=game_id
-    )
+    round_obj = Round(id=str(uuid.uuid4()), round_number=game.current_round, game_id=game_id)
+    bid_list = ["0", "0", "0"]
+    bid_list[(game.current_round + 1) % 3] = "100"  # player before bid_starter should have bid of 100
+
+    round_obj.bids_list = bid_list
+    round_obj.bid_starter = (game.current_round + 2) % 3
     round_obj.talon_list = talon
     session.add(round_obj)
     session.commit()
@@ -107,35 +111,100 @@ def deal_cards(players: List[Player]):
         players[player_index].cards_init_list.append(deck[i])
         players[player_index].cards_current_list.append(deck[i])
 
+    for player in players:
+        player.assign_max_biddable_amount()
+
     talon = deck[-3:]
 
     return players, talon
 
 
-def make_bid(game_id: str, player_local_id: int, bid: int):
-    """TODO: Write docstring"""
+def make_bid(game_id: str, player_id: str, bid: int):
+    """Bidding function"""
     session = Session()
 
-    curr_round_obj = get_current_round_from_db(session, game_id)
-    if not curr_round_obj:
+    game = session.query(Game).filter_by(id=game_id).first()
+
+    if game.game_state != GameState.BIDDING.value:
         session.close()
-        return "Round not found"
+        return "Not in bidding game state"
+
+    curr_round_obj = get_current_round_from_db(session, game_id)
+    players = get_players_with_game_from_db(session, game)
+
+    if not curr_round_obj or len(players) != 3:
+        session.close()
+        return "Round or player not found"
+
+    player_local_id = next((index for index, player in enumerate(players) if player.id == player_id), None)
+
+    if curr_round_obj.bids_list[player_local_id] == "-1":
+        session.close()
+        return "The player already passed bidding"
 
     next_player_bid = int(curr_round_obj.bids_list[(player_local_id + 1) % 3])
     prev_player_bid = int(curr_round_obj.bids_list[(player_local_id + 2) % 3])
 
     if bid <= next_player_bid or bid <= prev_player_bid:
         session.close()
-        return "Less than biddable amount"
+        return "Less than other players' bids"
+
+    if bid > players[player_local_id].max_biddable_amount:
+        session.close()
+        return "More than maximum biddable amount"
 
     temp_bids_list = curr_round_obj.bids_list
     temp_bids_list[player_local_id] = str(bid)
+
+    next_player = players[(player_local_id + 1) % 3]
+    prev_player = players[(player_local_id + 2) % 3]
+
+    if bid >= next_player.max_biddable_amount:
+        temp_bids_list[(player_local_id + 1) % 3] = "-1"
+
+    if bid >= prev_player.max_biddable_amount:
+        temp_bids_list[(player_local_id + 2) % 3] = "-1"
+
     curr_round_obj.bids_list = temp_bids_list
+
+    if temp_bids_list.count("-1") >= 2:
+        game.game_state = GameState.TALON.value
+        curr_round_obj.bid_winner = next((index for index, value in enumerate(temp_bids_list) if value != "-1"), None)
+
     session.add(curr_round_obj)
     session.commit()
     session.close()
 
     return "Bid made successfully"
+
+
+def pass_bid(game_id: str, player_id: str):
+    """Passing function during bidding"""
+    session = Session()
+
+    game = session.query(Game).filter_by(id=game_id).first()
+    curr_round_obj = get_current_round_from_db(session, game_id)
+    bidding_player = get_player_from_db(session, player_id)
+
+    if not curr_round_obj or not bidding_player:
+        session.close()
+        return "Round or player not found"
+
+    player_local_id = bidding_player.local_id
+
+    temp_bids_list = curr_round_obj.bids_list
+    temp_bids_list[player_local_id] = str(-1)  # -1 for pass
+    curr_round_obj.bids_list = temp_bids_list
+
+    if temp_bids_list.count("-1") >= 2:
+        game.game_state = GameState.TALON.value
+        curr_round_obj.bid_winner = next((index for index, value in enumerate(temp_bids_list) if value != "-1"), None)
+
+    session.add(curr_round_obj)
+    session.commit()
+    session.close()
+
+    return "Passed bidding successfully"
 
 
 def take_three_cards(player, card):
